e32c18a1d7a213c4c686affd1815ca59b70301e7
Skobayashi <suguru@iii-planning.com>
2013-09-27 16:41:04 +0900

    検索インデックス作成処理の修正

--- library/Cast/Commands/SearchIndex.php
+++ library/Cast/Commands/SearchIndex.php
@@ -20,7 +20,13 @@ class SearchIndex extends Base\AbstractCommand
         try {
             set_time_limit(0);
 
-            mb_internal_encoding('UTF-8');
+            if (isset($params[0]) && $params[0] == 'remote') {
+                $isAWS = true;
+            } else {
+                $isAWS = false;
+            }
+
+
             $this->initDatabaseConnection();
 
             $S3 = new S3();
@@ -31,45 +37,91 @@ class SearchIndex extends Base\AbstractCommand
             $casts = $cast_model->query->notSearchIndexCasts();
 
             foreach ($casts as $cast) {
-                // キャスト名を分解する
-                $names = $this->_decompositionValue($cast->name);
+                $cast_model->setRecord($cast);
 
-                // ふりがなを分解する
-                $furiganas = $this->_decompositionValue($cast->furigana);
-
-                $values = array_merge($names, $furiganas);
+                // キャスト名、ふりがなを解析する
+                $values = $cast_model->decompositionValue();
 
 
-                // 検索情報をS3に保存する
+                // 検索情報を保存する
                 foreach ($values as $value) {
                     try {
                         $db = \Zend_Registry::get('db');
                         $db->beginTransaction();
                     
-                        // 既にresult.jsonがS3にあるかどうかを確認する
-                        $response = $S3->getSearchIndex($value);
 
-                        if ($response->status == 200) {
-                            $json = json_decode($response->body);
+                        if ($isAWS) {
+                            // 既にresult.jsonがS3にあるかどうかを確認する
+                            $response = $S3->getSearchIndex($value);
 
-                            if (! in_array($cast->name, $json)) {
-                                $json[] = $cast->name;
+                            if ($response->status == 200) {
+                                $json = json_decode($response->body);
+                                $names = array();
+
+                                foreach ($json as $val) {
+                                    if (! in_array($val->name, $names)) {
+                                        $names[] = $val->name;
+                                    }
+                                }
+
+                                if (! in_array($value, $names)) {
+                                    $json[] = array('name' => $value);
+                                }
+
+                                $json = json_encode($json);
+
+                            } else {
+                                $json = array($cast->name);
                             }
 
+
+                            // 検索インデックスをS3に保存する
+                            $response = $S3->uploadSearchIndex($value, $json);
+
+                            if ($response->isOK()) {
+                                $cast_model->set('search_index', true);
+                                $cast_model->update();
+                            }
+
+
                         } else {
-                            $json = array($cast->name);
+                            // ローカル保存の場合
+                            mb_internal_encoding('UTF-8');
+
+                            $length = mb_strlen($value);
+                            $json_path = ROOT_PATH.'/resources/json/search/'.$length.'/'.$value;
+                            if (! is_dir($json_path)) {
+                                mkdir($json_path, 0755, true);
+                            }
+
+
+                            $json_path .= '/result.json';
+                            if (file_exists($json_path)) {
+                                $json = json_decode(file_get_contents($json_path));
+                                $names = array();
+
+                                foreach ($json as $val) {
+                                    if (! in_array($val->name, $names)) {
+                                        $names[] = $val->name;
+                                    }
+                                }
+
+                                if (! in_array($cast_model->get('name'), $names)) {
+                                    $json[] = array('name' => $cast_model->get('name'));
+                                }
+                                $json = json_encode($json);
+                            
+                            } else {
+                                touch($json_path);
+                                $json = json_encode(array(array('name' => $cast_model->get('name'))));
+                            }
+
+                            $fp = fopen($json_path, 'w');
+                            fwrite($fp, $json);
+                            fclose($fp);
+                            unset($fp);
                         }
 
-
-                        // 検索インデックスをS3に保存する
-                        $response = $S3->uploadSearchIndex($value, $json);
-
-                        if ($response->isOK()) {
-                            $cast_model->setRecord($cast);
-                            $cast_model->set('search_index', true);
-                            $cast_model->update();
-                        }
-                        
                         $db->commit();
                     
                     } catch (\Exception $e) {
@@ -87,38 +139,13 @@ class SearchIndex extends Base\AbstractCommand
 
 
     /**
-     * 引数の文字列をそれぞれに分解して配列で返す
-     *
-     * @param string $value  分解する文字列
-     * @author app2641
-     **/
-    private function _decompositionValue ($value)
-    {
-        $data = array();
-        $count = mb_strlen($value);
-
-        // 最低文字列2から文字数分ループ処理を行う 
-        for ($i = 2; $i <= $count; $i++) {
-
-            // substr始点から$i分の文字列を切り取る
-            for ($r = 0; $r <= ($count - $i); $r++) {
-                $data[] = mb_substr($value, $r, $i, 'UTF-8');
-            }
-        }
-
-        return $data;
-    }
-
-
-
-    /**
      * コマンドリストに表示するヘルプメッセージを表示する
      *
      **/
     public static function help ()
     {
         /* write help message */
-        $msg = 'キャストの検索インデックスデータを作成する';
+        $msg = 'キャストの検索インデックスデータを作成する。第一引数にremote指定でS3保存。その他はローカル保存。';
 
         return $msg;
     }
--- library/Cast/Model/CastModel.php
+++ library/Cast/Model/CastModel.php
@@ -16,4 +16,32 @@ class CastModel extends AbstractModel
         $container = new Container(new ModelFactory);
         $this->query = $container->get('CastQuery');
     }
+
+
+
+    /**
+     * キャスト名とふりがなを解析して検索インデックス用の配列にする
+     *
+     * @author app2641
+     **/
+    public function decompositionValue ()
+    {
+        mb_internal_encoding('UTF-8');
+        $data  = array();
+
+        foreach (array($this->get('name'), $this->get('furigana')) as $value) {
+            $length = mb_strlen($value);
+
+            // 最低文字列2から文字数分ループ処理を行う 
+            for ($i = 2; $i <= $length; $i++) {
+
+                // substr始点から$i分の文字列を切り取る
+                for ($r = 0; $r <= ($length - $i); $r++) {
+                    $data[] = mb_substr($value, $r, $i, 'UTF-8');
+                }
+            }
+        }
+
+        return $data;
+    }
 }
